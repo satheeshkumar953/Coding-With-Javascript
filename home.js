3db0e5604d750e93a0304242316a798e26104d45
Satheesh_Kumar <128807360+satheeshkumar953@users.noreply.github.com>
2025-03-06 10:07:02 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,7 +1,4 @@
 // JavaScript String Length.
-
-const { useCallback } = require("react");
-
 // The length property returns the length of a string:
 let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let length = text.length;
